564825d97f7630b1aaba4ab1ffc3617dd7089213
amaieras <maieras.andrei@gmail.com>
2018-04-17 23:07:37 +0300

    [DEV] Learn other tags

---
+++ js/script.js
@@ -0,0 +1,6 @@
+
+var changeColor= function() {
+	var h1Element= document.getElementById("pemine");
+	h1Element.style.color = "pink";
+	console.log("Sunt jmechera");
+	}
--- script.js
+++
@@ -1,6 +0,0 @@
-
-var changeColor= function() {
-	var h1Element= document.getElementById("pemine");
-	h1Element.style.color = "pink";
-	console.log("Sunt jmechera");
-	}
